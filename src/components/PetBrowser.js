9818755a6adc531df72d8b4feea4dee974d34e77
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2024-03-31 23:38:05 -0700

    adds test defined functionality

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
+import Pet from './Pet';
 
-import Pet from "./Pet";
-
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({ pets, onAdoptPet }) {
+  if (pets) {
+    return pets.map((pet) => {
+      return (
+        <div key={pet.id} className='ui cards'>
+          <Pet pet={pet} onAdoptPet={onAdoptPet} />
+        </div>
+      );
+    });
+  } else {
+    return (
+      <div className='ui cards'>Pet cards failed to load. Please Refresh.</div>
+    );
+  }
 }
 
 export default PetBrowser;
