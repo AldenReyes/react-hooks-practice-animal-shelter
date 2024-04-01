8838f7ba1011210278b998ea9e6003a3814256bc
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2024-03-31 23:37:47 -0700

    Adds test defined functionality

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React from 'react';
 
-function Filters() {
+function Filters({ onChangeType, onFindPetsClick }) {
   return (
-    <div className="ui form">
+    <div className='ui form'>
       <h3>Animal type</h3>
-      <div className="field">
-        <select name="type" id="type" aria-label="type">
-          <option value="all">All</option>
-          <option value="cat">Cats</option>
-          <option value="dog">Dogs</option>
-          <option value="micropig">Micropigs</option>
+      <div className='field'>
+        <select
+          name='type'
+          id='type'
+          aria-label='type'
+          onChange={(e) => onChangeType(e)}
+        >
+          <option value='all'>All</option>
+          <option value='cat'>Cats</option>
+          <option value='dog'>Dogs</option>
+          <option value='micropig'>Micropigs</option>
         </select>
       </div>
-
-      <div className="field">
-        <button className="ui secondary button">Find pets</button>
+      <div className='field'>
+        <button className='ui secondary button' onClick={onFindPetsClick}>
+          Find pets
+        </button>
       </div>
     </div>
   );
